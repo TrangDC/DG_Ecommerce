98b67dbe712a2e79f4cd2e44f97f41828afcbac8
TrangDC <dinhcongtrang99@gmail.com>
2024-02-16 10:49:32 +0700

    create route to home, shop, cart element. fixed header and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
+import Home from "./pages/HomePage/Home";
+import Shop from "./pages/ShopPage/Shop";
+import Cart from "./pages/CartPage/Cart";
+import Header from "./common/Header";
+import Footer from "./common/Footer";
 
 function App() {
   return (
     <div className="App">
-
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+               <Route path="/" element={<Home />} />
+               <Route path="/shop" element={<Shop />} />
+               <Route path="/cart" element={<Cart />} />
+            </Routes>
+            <Footer/>
+        </BrowserRouter>
     </div>
   );
 }
