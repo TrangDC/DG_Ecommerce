d2863cf746a679415dce9854436819704fb896bf
TrangDC <dinhcongtrang99@gmail.com>
2024-02-16 10:52:41 +0700

    add link to shop, cart, home on header element

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Header = () => {
     return (
         <div>
-            Header
+            <Link to={"/"}>Home</Link>
+            <Link to={"/shop"}>Shop</Link>
+            <Link to={"/cart"}>Cart</Link>
         </div>
     );
 };
