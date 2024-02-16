746f874ec5ea36029faadb97f8cfc2f2c0c1f4f4
TrangDC <dinhcongtrang99@gmail.com>
2024-02-16 16:04:49 +0700

    add nav button with color. need to change later

--- src/common/Header.js
+++ src/common/Header.js
@@ -5,6 +5,7 @@ import TopBar from "./TopBar";
 const Header = () => {
 
     const [isSticky, setIsSticky] = useState(false);
+    const [toggler, setToggler] = useState(false);
 
     useEffect(() => {
         window.addEventListener("scroll", handleScroll);
@@ -28,23 +29,29 @@ const Header = () => {
                             ${ isSticky ? "sticky-header": ""}`}>
                 <div className="row px-xl-5">
                     Category
-                    <div>
-                        <nav>
+                    <div className="col-lg-9">
+                        <nav className="navbar navbar-expand-lg bg-dark nav-dark py-3 py-lg-0 px-0">
                             <Link className="text-decoration-none d-block d-lg-none" to={"/"}>
                                 <span className="h1 text-uppercase text-primary bg-dark px-2">
                                     Multi
                                 </span>
-                                <span className="h1 text-uppercase text-dark bg-primary px-2">
+                                <span className="h1 text-uppercase text-dark bg-primary px-2 ml-n1">
                                     Shop
                                 </span>
                             </Link>
+                            <button type="button"
+                                    className="navbar-toggler bg-success"
+                                    onClick={() => setToggler(!toggler)}>
+                                <span className="navbar-toggler-icon">
+                                </span>
+                            </button>
                         </nav>
                     </div>
                 </div>
             </div>
-            <Link to={"/"}>Home</Link>
-            <Link to={"/shop"}>Shop</Link>
-            <Link to={"/cart"}>Cart</Link>
+            {/*<Link to={"/"}>Home</Link>*/}
+            {/*<Link to={"/shop"}>Shop</Link>*/}
+            {/*<Link to={"/cart"}>Cart</Link>*/}
         </div>
     );
 };
