f58f44f7fddc1cc4644768d2da511f2f50aa71b3
TrangDC <dinhcongtrang99@gmail.com>
2024-02-16 22:00:21 +0700

    create and add Features element to Home

---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <div>
+            {/* Your component code here */}
+        </div>
+    );
+};
+
+export default Sidebar;
