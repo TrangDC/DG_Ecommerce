310f4fe6fa2610e5378f1ea2fe6bab936eb2bd3c
TrangDC <dinhcongtrang99@gmail.com>
2024-02-16 21:16:46 +0700

    add title and description to each image in slider

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,6 +3,7 @@ import {carousel} from "../data/Data";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import {Link} from "react-router-dom";
 
 const Banner = () => {
     const settings = {
@@ -29,12 +30,21 @@ const Banner = () => {
                                             className="position-absolute w-100 h-100"
                                             src={value.cover_img} alt="img"
                                         />
+                                        <div className="carousel-caption d-flex flex-column align-items-center justify-content-between">
+                                            <div className="p-3">
+                                                <h1 className="display-4 text-white mb-3">{value.title}</h1>
+                                                <p className="mx-md-5 px-5">{value.description}</p>
+                                                <Link className={"btn btn-outline-light py-2 px-4 mt-3"}>{value.btn}</Link>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             ))}
                         </Slider>
                     </div>
                 </div>
+
+                <div></div>
             </div>
         </div>
     );
