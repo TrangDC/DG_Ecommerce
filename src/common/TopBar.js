2b5ab5c2aed5472ecdd92225c750e7a4bd03ffc6
TrangDC <dinhcongtrang99@gmail.com>
2024-02-16 11:09:18 +0700

    add css and img file

--- src/common/TopBar.js
+++ src/common/TopBar.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 const TopBar = () => {
     return (
-        <div>
-            TopBar
+        <div className="container-fluid">
+            <div className={"row bg-secondary py-1 px-xl-5"}>
+                <div>
+                    <div>
+                        {
+
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
