218a1fbb578b08599d71a81490ec5fd2aafa8cc8
TrangDC <dinhcongtrang99@gmail.com>
2024-02-16 12:16:40 +0700

    add nav on the left. set topdropdown near the right

--- src/common/TopBar.js
+++ src/common/TopBar.js
@@ -7,7 +7,13 @@ const TopBar = () => {
         <div className="container-fluid">
             <div className={"row bg-secondary py-1 px-xl-5"}>
                 <div className="col-lg-6 d-none d-lg-block">
-
+                    <div className="d-inline-flex align-items-center h-100">
+                        {nav.slice(2, 6).map((top, index) => (
+                            <Link to={top.path} className="text-body mr-3" key={index}>
+                                {top.text}
+                            </Link>
+                        ))}
+                    </div>
                 </div>
 
                 <div className={"col-lg-6 text-center d-lg-block"}>
@@ -22,6 +28,14 @@ const TopBar = () => {
                             ))
                         }
                     </div>
+
+                    <div>
+                        <Link className="btn px-0 ml-2" to="/">
+                            <i className="fas fa-heart text-dark"></i>
+                            <span></span>
+                        </Link>
+                    </div>
+
                 </div>
             </div>
         </div>
