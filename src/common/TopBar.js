85afc6bc35448a8f4a7920a88aa88bc7c255fa0f
TrangDC <dinhcongtrang99@gmail.com>
2024-02-16 11:22:18 +0700

    display  nav from data.js using slice and map

--- src/common/TopBar.js
+++ src/common/TopBar.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import {nav} from "../data/Data";
+import {Link} from "react-router-dom";
 
 const TopBar = () => {
     return (
         <div className="container-fluid">
             <div className={"row bg-secondary py-1 px-xl-5"}>
-                <div>
-                    <div>
-                        {
+                <div className="col-lg-6 d-none d-lg-block">
 
+                </div>
+
+                <div className={"col-lg-6 d-lg-block"}>
+                    <div className="d-inline-flex align-items-center h-100">
+                        {
+                            nav.slice(2, 5).map((value, index) => (
+                                <Link key={index} className={"text-body mr-3"}>
+                                    {value.text}
+                                </Link>
+                            ))
                         }
                     </div>
                 </div>
