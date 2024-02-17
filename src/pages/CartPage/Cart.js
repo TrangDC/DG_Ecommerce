15f0a1e93e9faf5bacb2bb1343b6a5d5a309b2a5
TrangDC <dinhcongtrang99@gmail.com>
2024-02-17 10:21:58 +0700

    edit Cart element. Show list of product add to cart, handle method increase, decrease qty, remove product from cart using redux.

--- src/pages/CartPage/Cart.js
+++ src/pages/CartPage/Cart.js
@@ -1,6 +1,39 @@
 import React from 'react';
+import {products} from "../../data/Data";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faMinus, faPlus, faTimes} from "@fortawesome/free-solid-svg-icons";
+import {useDispatch, useSelector} from "react-redux";
+import {removeItem, updateQuantity} from "../../redux/CartSlice";
 
 const Cart = () => {
+
+    const dispatch = useDispatch();
+    const increaseQty = (cartProductId, currentQty) => {
+        const newQty = currentQty + 1;
+        dispatch(updateQuantity({
+            id: cartProductId,
+            quantity: newQty
+        }))
+    };
+
+    const decreaseQty = (cartProductId, currentQty) => {
+        const newQty = Math.max(currentQty - 1, 1);
+        dispatch(updateQuantity({
+            id: cartProductId,
+            quantity: newQty
+        }))
+    };
+
+    const {
+        data: cartProducts,
+        totalAmounts,
+        deliveryCharge,
+    } = useSelector((state) => state.cart);
+
+    const handleRemoveItem = (itemId) => {
+        dispatch(removeItem({id: itemId}));
+    }
+
     return (
         <>
             <div className="container-fluid">
@@ -16,7 +49,68 @@ const Cart = () => {
                                 <th>Remove</th>
                             </tr>
                             </thead>
-                            <tbody></tbody>
+                            <tbody>
+                            {
+                                cartProducts.map((value, index) => (
+                                    <tr>
+                                        <td className="align-middle">
+                                            <img
+                                                src={value.product_img}
+                                                alt={"img"}
+                                                style={{width: "50px"}}
+                                            />
+                                            {value.product_name}
+                                        </td>
+                                        <td className="align-middle">
+                                            {value.price}
+                                        </td>
+                                        <td className="align-middle">
+                                            <div className="input-group quantity mx-auto">
+                                                <div className="input-group-btn">
+                                                    <button
+                                                        className="btn btn-sm btn-primary btn-minus"
+                                                        type="button"
+                                                        onClick={() => decreaseQty(value.id, value.quantity)}
+                                                    >
+                                                      <FontAwesomeIcon icon={faMinus}></FontAwesomeIcon>
+                                                    </button>
+                                                </div>
+
+                                                <input
+                                                    type="text"
+                                                    name="text"
+                                                    value={value.quantity || 1}
+                                                    className="form-control form-control-sm bg-secondary border-0 text-center"
+                                                />
+
+                                                <div className="input-group-btn">
+                                                    <button
+                                                        className="btn btn-sm btn-primary btn-plus"
+                                                        type="button"
+                                                        onClick={() => increaseQty(value.id, value.quantity)}
+                                                    >
+                                                        <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+                                                    </button>
+                                                </div>
+
+                                            </div>
+                                        </td>
+                                        <td className="align-middle">
+                                            {value.totalPrice}
+                                        </td>
+                                        <td className="align-middle">
+                                            <button
+                                                type="button"
+                                                className="btn btn-sm btn-danger"
+                                                onClick={() => handleRemoveItem(value.id)}
+                                            >
+                                                <FontAwesomeIcon icon={faTimes}></FontAwesomeIcon>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
+                            </tbody>
                         </table>
                     </div>
                 </div>
