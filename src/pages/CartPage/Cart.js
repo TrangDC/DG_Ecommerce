df5fa826bff63527a14a06313b2e8550dca74955
TrangDC <dinhcongtrang99@gmail.com>
2024-02-17 10:31:43 +0700

    add useEffect to update number of item on cart icon. add method to show message if cart empty.

--- src/pages/CartPage/Cart.js
+++ src/pages/CartPage/Cart.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {products} from "../../data/Data";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faMinus, faPlus, faTimes} from "@fortawesome/free-solid-svg-icons";
 import {useDispatch, useSelector} from "react-redux";
-import {removeItem, updateQuantity} from "../../redux/CartSlice";
+import {getCartTotal, removeItem, updateQuantity} from "../../redux/CartSlice";
 
 const Cart = () => {
 
@@ -34,89 +34,103 @@ const Cart = () => {
         dispatch(removeItem({id: itemId}));
     }
 
+    useEffect(() => {
+        dispatch(getCartTotal())
+    }, [useSelector((state) => state.cart)]);
+
+    const emptyCartMsg = (
+        <h4 className="container text-center p-4">Your Cart is Empty</h4>
+    )
+
     return (
         <>
-            <div className="container-fluid">
-                <div className="row px-xl-5">
-                    <div className="col-lg-8 table-responsive mb-5">
-                        <table className="table table-light table-borderless text-center mb-0">
-                            <thead className="thead-dark">
-                            <tr>
-                                <th>Product name</th>
-                                <th>Price</th>
-                                <th>Quantity</th>
-                                <th>Total</th>
-                                <th>Remove</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            {
-                                cartProducts.map((value, index) => (
+            {cartProducts.length === 0 ?
+                (
+                    emptyCartMsg
+                ) : (
+                    <div className="container-fluid">
+                        <div className="row px-xl-5">
+                            <div className="col-lg-8 table-responsive mb-5">
+                                <table className="table table-light table-borderless text-center mb-0">
+                                    <thead className="thead-dark">
                                     <tr>
-                                        <td className="align-middle">
-                                            <img
-                                                src={value.product_img}
-                                                alt={"img"}
-                                                style={{width: "50px"}}
-                                            />
-                                            {value.product_name}
-                                        </td>
-                                        <td className="align-middle">
-                                            {value.price}
-                                        </td>
-                                        <td className="align-middle">
-                                            <div className="input-group quantity mx-auto">
-                                                <div className="input-group-btn">
-                                                    <button
-                                                        className="btn btn-sm btn-primary btn-minus"
-                                                        type="button"
-                                                        onClick={() => decreaseQty(value.id, value.quantity)}
-                                                    >
-                                                      <FontAwesomeIcon icon={faMinus}></FontAwesomeIcon>
-                                                    </button>
-                                                </div>
-
-                                                <input
-                                                    type="text"
-                                                    name="text"
-                                                    value={value.quantity || 1}
-                                                    className="form-control form-control-sm bg-secondary border-0 text-center"
-                                                />
-
-                                                <div className="input-group-btn">
-                                                    <button
-                                                        className="btn btn-sm btn-primary btn-plus"
-                                                        type="button"
-                                                        onClick={() => increaseQty(value.id, value.quantity)}
-                                                    >
-                                                        <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
-                                                    </button>
-                                                </div>
-
-                                            </div>
-                                        </td>
-                                        <td className="align-middle">
-                                            {value.totalPrice}
-                                        </td>
-                                        <td className="align-middle">
-                                            <button
-                                                type="button"
-                                                className="btn btn-sm btn-danger"
-                                                onClick={() => handleRemoveItem(value.id)}
-                                            >
-                                                <FontAwesomeIcon icon={faTimes}></FontAwesomeIcon>
-                                            </button>
-                                        </td>
+                                        <th>Product name</th>
+                                        <th>Price</th>
+                                        <th>Quantity</th>
+                                        <th>Total</th>
+                                        <th>Remove</th>
                                     </tr>
-                                ))
-                            }
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </>
+                                    </thead>
+                                    <tbody>
+                                    {
+                                        cartProducts.map((value, index) => (
+                                            <tr>
+                                                <td className="align-middle">
+                                                    <img
+                                                        src={value.product_img}
+                                                        alt={"img"}
+                                                        style={{width: "50px"}}
+                                                    />
+                                                    {value.product_name}
+                                                </td>
+                                                <td className="align-middle">
+                                                    {value.price}
+                                                </td>
+                                                <td className="align-middle">
+                                                    <div className="input-group quantity mx-auto">
+                                                        <div className="input-group-btn">
+                                                            <button
+                                                                className="btn btn-sm btn-primary btn-minus"
+                                                                type="button"
+                                                                onClick={() => decreaseQty(value.id, value.quantity)}
+                                                            >
+                                                                <FontAwesomeIcon icon={faMinus}></FontAwesomeIcon>
+                                                            </button>
+                                                        </div>
 
+                                                        <input
+                                                            type="text"
+                                                            name="text"
+                                                            value={value.quantity || 1}
+                                                            className="form-control form-control-sm bg-secondary border-0 text-center"
+                                                        />
+
+                                                        <div className="input-group-btn">
+                                                            <button
+                                                                className="btn btn-sm btn-primary btn-plus"
+                                                                type="button"
+                                                                onClick={() => increaseQty(value.id, value.quantity)}
+                                                            >
+                                                                <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+                                                            </button>
+                                                        </div>
+
+                                                    </div>
+                                                </td>
+                                                <td className="align-middle">
+                                                    {value.totalPrice}
+                                                </td>
+                                                <td className="align-middle">
+                                                    <button
+                                                        type="button"
+                                                        className="btn btn-sm btn-danger"
+                                                        onClick={() => handleRemoveItem(value.id)}
+                                                    >
+                                                        <FontAwesomeIcon icon={faTimes}></FontAwesomeIcon>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        ))
+                                    }
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+
+        </>
     );
 };
 
