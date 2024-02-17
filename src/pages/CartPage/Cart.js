e494594cccf2b50a6796ff0b1f0cf3836e112656
TrangDC <dinhcongtrang99@gmail.com>
2024-02-17 10:46:20 +0700

    add cart total price and shipping card

--- src/pages/CartPage/Cart.js
+++ src/pages/CartPage/Cart.js
@@ -125,8 +125,57 @@ const Cart = () => {
                                     </tbody>
                                 </table>
                             </div>
+                            <div className="col-lg-4">
+                                <form className="mb-30" action="">
+                                    <div className="input-group">
+                                        <input
+                                            type="text"
+                                            className="form-control border-0 p-4"
+                                            placeholder="Coupon Code"
+                                        />
+                                        <div className="input-group-append">
+                                            <button className="btn btn-primary">Apply Coupon</button>
+                                        </div>
+                                    </div>
+                                </form>
+                                <h5 className="section-title position-relative text-uppercase mb-3">
+                                    <span className="bg-secondary pr-3">Cart Summary</span>
+                                </h5>
+                                <div className="bg-light p-30 mb-5">
+                                    <div className="border-bottom pb-2">
+                                        <div className="d-flex justify-content-between mb-3">
+                                            <h6>Subtotal</h6>
+                                            <h6>{totalAmounts}</h6>
+                                        </div>
+                                        <div className="d-flex justify-content-between">
+                                            <h6 className="font-weight-medium">Shipping</h6>
+                                            <h6 className="font-weight-medium">{deliveryCharge}</h6>
+                                        </div>
+                                    </div>
+                                    <div className="pt-2">
+                                        <div className="d-flex justify-content-between mt-2">
+                                            <h5>Total</h5>
+                                            <h5>{totalAmounts + deliveryCharge}</h5>
+                                        </div>
+                                        <button className="btn btn-block btn-primary font-weight-bold my-3 py-3">
+                                            Proceed To Checkout
+                                        </button>
+
+                                        {/* <StripeCheckout
+                    token={onToken}
+                    stripeKey={publishableKey}
+                    amount={100 * totalAmounts}
+                    name="MD"
+                    currency="USD"
+                    label="Proceed Checkout"
+                    className="btn btn-block btn-primary font-weight-bold my-3 py-3"
+                  /> */}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
+
                 )
             }
 
